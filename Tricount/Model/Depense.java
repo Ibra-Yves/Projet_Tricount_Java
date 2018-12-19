acbd55794ac3449a2ae20b64c51049a7b860ce59
RemyVase <44263729+RemyVase@users.noreply.github.com>
2018-12-19 09:00:28 +0100

    Update Depense
    Mise à jour finale de Depense.java

--- Tricount/Model/Depense.java
+++ Tricount/Model/Depense.java
@@ -6,15 +6,29 @@ public class Depense {
 	private int idPayeur;
 	private double montant;
 	private int[] profiteurs;
+	private String date;
 	
 	public Depense(int idPayeur, double montant, int[] profiteurs) {
 		this.idPayeur= idPayeur;
 		this.montant= montant;
 		this.profiteurs = profiteurs;
-		
+		for (int i = 0; i < profiteurs.length; i++) {
+			if(profiteurs[i] == idPayeur) {
+				throw new IllegalArgumentException("Le payeur ne peut pas Ítre dans les profiteurs");
+			}
+		}
+	}
+	
+	public Depense(int idPayeur, double montant, int[] profiteurs,String date) {
+		this(idPayeur, montant, profiteurs);
+		this.date = date;
 	}
 	
 	
+	public String getDate() {
+		return date;
+	}
+
 	public int getIdPayeur() {
 		return idPayeur;
 	}
@@ -30,6 +44,9 @@ public class Depense {
 	}
 
 
+	/**
+	 * @return renvoie la liste des modifications de solde aprËs une dÈpense
+	 */
 	public ArrayList<ModificationSolde> getModifsSolde()
 	{
 		double partieMontant = montant/(profiteurs.length+1);
